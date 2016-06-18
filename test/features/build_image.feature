6a433aab114ff27ce054fc0f70672a5859a2b504
arrawatia <sumit.arrawatia@gmail.com>
2016-06-17 19:13:19 -0700

    More tests.

--- test/features/build_image.feature
+++ test/features/build_image.feature
@@ -3,3 +3,17 @@ Feature: build CP base docker containers
   Scenario: build a base image
     When I build confluentinc/base from debian/base
     Then a confluentinc/base image should exist
+
+  Scenario: Check if base image has java 8 installed
+    Given image confluentinc/base exists
+    When I run java -version on confluentinc/base
+    Then output should have java version "1.8.0_91"
+
+  Scenario: Check if base image has dub installed
+    Given image confluentinc/base exists
+    Then path /usr/local/bin/dub should exist in image confluentinc/base
+
+  Scenario: build a zk image
+    Given image confluentinc/base exists
+    When I build confluentinc/zk from debian/zk
+    Then a confluentinc/zk image should exist
--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -5,6 +5,18 @@ import os
 IMAGE_DIR = os.path.join(os.getcwd(), "..")
 
 
+def run_docker_command(image, cmd):
+    client = docker.from_env(assert_hostname=False)
+    c = client.create_container(image="%s:latest" % (image,), command=str(cmd))
+    client.start(c)
+    client.wait(c)
+    logs = client.logs(c)
+    client.remove_container(c)
+    print("Ran command %s on image %s." % (cmd, image))
+    print("Output :  %s " % (logs,))
+    return logs
+
+
 @when(u'I build {image_name} from {basedir}')
 def step_impl(context, image_name, basedir):
     client = docker.from_env(assert_hostname=False)
@@ -14,8 +26,30 @@ def step_impl(context, image_name, basedir):
     return True
 
 
+@given(u'image {image_name} exists')
 @then(u'a {image_name} image should exist')
 def step_impl(context, image_name):
     client = docker.from_env(assert_hostname=False)
     tags = [t for image in client.images() for t in image['RepoTags']]
     assert("%s:latest" % (image_name,) in tags)
+
+
+@when(u'I run {cmd} on {image}')
+def step_impl(context, cmd, image):
+    context.logs = run_docker_command(image, cmd)
+
+
+@then(u'I should get {output}')
+def step_impl(context, output):
+    assert(output == context.logs)
+
+
+@then(u'output should have {output}')
+def step_impl(context, output):
+    assert(output in context.logs)
+
+
+@then(u'path {path} should exist in image {image}')
+def step_impl(context, path, image):
+    cmd = "bash -c '[ -e %s ] || echo fail' " % (path,)
+    assert("fail" not in run_docker_command(image, cmd))
