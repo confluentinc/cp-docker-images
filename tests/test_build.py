032b9f45991f46f974eebdd2227a0070aacc8361
Samuel Julius Hecht <samjhecht@gmail.com>
2016-07-20 15:58:55 -0700

    adding SR tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -29,7 +29,6 @@ class BaseImageTest(unittest.TestCase):
         self.assertTrue(utils.path_exists_in_image(self.image, "/usr/local/bin/dub"))
         self.assertTrue(utils.path_exists_in_image(self.image, "/usr/local/bin/cub"))
 
-
 class ZookeeperImageTest(unittest.TestCase):
 
     def setUp(self):
@@ -83,7 +82,35 @@ class KafkaImageTest(unittest.TestCase):
         self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/launch"))
         self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/run"))
 
-
     def test_kafka_commands(self):
         expected = "USAGE: /usr/bin/kafka-server-start [-daemon] server.properties [--override property=value]*"
         self.assertTrue(expected in utils.run_docker_command(image=self.image, command="kafka-server-start"))
+
+class SchemaRegistryImageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.image = "confluentinc/schema-registry"
+        utils.build_image(self.image, get_dockerfile_path("debian/base"))
+        utils.build_image(self.image, get_dockerfile_path("debian/schema-registry"))
+
+    def test_image_build(self):
+        self.assertTrue(utils.image_exists(self.image))
+
+    def test_zk_install(self):
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/kafka"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/schema-registry"))
+
+    def test_boot_scripts_present(self):
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/configure"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/ensure"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/launch"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/run"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/configure"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/ensure"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/launch"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/run"))
+
+    def test_schema_registry_commands(self):
+        expected = "ERROR Properties file is required to start the schema registry REST instance (io.confluent.kafka.schemaregistry.rest.SchemaRegistryMain:38)"
+        self.assertTrue(expected in utils.run_docker_command(image=self.image, command="schema-registry-start"))
