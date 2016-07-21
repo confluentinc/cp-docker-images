8c37a8cce0365d31e458d2305e49c312ef98dd84
Roger Hoover <roger.hoover@gmail.com>
2016-07-21 16:29:37 -0700

    Added default config test

--- tests/test_control_center.py
+++ tests/test_control_center.py
@@ -9,11 +9,14 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_DIR = os.path.join(CURRENT_DIR, "fixtures", "debian", "control-center")
 KAFKA_READY = "bash -c 'cub kafka-ready $ZOOKEEPER_CONNECT {brokers} 20 20 10 && echo PASS || echo FAIL'"
 ZK_READY = "bash -c 'cub zk-ready {servers} 10 10 2 && echo PASS || echo FAIL'"
+C3_CHECK = "bash -c 'dub wait {host} {port} 240 && curl -fs -X GET -i {host}:{port}/ && echo PASS || echo FAIL'"
+
 
 class ConfigTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        os.environ['DOCKER_CLIENT_TIMEOUT'] = "600"
         machine_name = os.environ["DOCKER_MACHINE_NAME"]
         cls.machine = utils.TestMachine(machine_name)
 
@@ -23,27 +26,37 @@ class ConfigTest(unittest.TestCase):
         assert "PASS" in cls.cluster.run_command_on_service("zookeeper", ZK_READY.format(servers="localhost:2181"))
         assert "PASS" in cls.cluster.run_command_on_service("kafka", KAFKA_READY.format(brokers=1))
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.cluster.shutdown()
-
     # @classmethod
-    # def is_schema_registry_healthy_for_service(cls, service):
-    #     output = cls.cluster.run_command_on_service(service, HEALTH_CHECK.format(host="localhost",port=8081))
-    #     assert "PASS" in output
+    # def tearDownClass(cls):
+    #     cls.cluster.shutdown()
+
+    @classmethod
+    def is_c3_healthy_for_service(cls, service):
+        output = utils.run_docker_command(
+            600,
+            image="confluentinc/control-center",
+            command=C3_CHECK.format(host=service, port=9021),
+            host_config={'NetworkMode': 'config-test_default'}
+        )
+        # output = cls.cluster.run_command_on_service(service, C3_CHECK.format(host="localhost", port=9021))
+        assert "PASS" in output
 
     def test_required_config_failure(self):
         self.assertTrue("BOOTSTRAP_SERVERS is required." in self.cluster.service_logs("failing-config", stopped=True))
         self.assertTrue("ZOOKEEPER_CONNECT is required." in self.cluster.service_logs("failing-config-missing-zk-connect", stopped=True))
         self.assertTrue("CONTROL_CENTER_REPLICATION_FACTOR is required." in self.cluster.service_logs("failing-config-missing-rep-factor", stopped=True))
 
-    # def test_default_config(self):
-    #     self.is_schema_registry_healthy_for_service("default-config")
-    #     props = self.cluster.run_command_on_service("default-config", "cat /etc/schema-registry/schema-registry.properties")
-    #     expected = """kafkastore.connection.url=zookeeper:2181/defaultconfig
-    #             host.name=default-config
-    #         """
-    #     self.assertEquals(props.translate(None, string.whitespace), expected.translate(None, string.whitespace))
+    def test_default_config(self):
+        self.is_c3_healthy_for_service("default-config")
+        props = self.cluster.run_command_on_service("default-config", "cat /etc/confluent-control-center/control-center.properties")
+        expected = """
+        bootstrap.servers=kafka:9092
+        zookeeper.connect=zookeeper:2181/defaultconfig
+        confluent.controlcenter.data.dir=/var/lib/confluent-control-center
+        confluent.monitoring.interceptor.topic.replication=1
+        confluent.controlcenter.internal.topics.replication=1
+        """
+        self.assertEquals(props.translate(None, string.whitespace), expected.translate(None, string.whitespace))
 
     # def test_default_logging_config(self):
     #     self.is_schema_registry_healthy_for_service("default-config")
