275a60daf22c8bc159d6cb404f507acc75d353ba
Roger Hoover <roger.hoover@gmail.com>
2016-07-21 16:34:42 -0700

    Fix up

--- tests/test_control_center.py
+++ tests/test_control_center.py
@@ -26,9 +26,9 @@ class ConfigTest(unittest.TestCase):
         assert "PASS" in cls.cluster.run_command_on_service("zookeeper", ZK_READY.format(servers="localhost:2181"))
         assert "PASS" in cls.cluster.run_command_on_service("kafka", KAFKA_READY.format(brokers=1))
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.cluster.shutdown()
+    @classmethod
+    def tearDownClass(cls):
+        cls.cluster.shutdown()
 
     @classmethod
     def is_c3_healthy_for_service(cls, service):
@@ -38,7 +38,6 @@ class ConfigTest(unittest.TestCase):
             command=C3_CHECK.format(host=service, port=9021),
             host_config={'NetworkMode': 'config-test_default'}
         )
-        # output = cls.cluster.run_command_on_service(service, C3_CHECK.format(host="localhost", port=9021))
         assert "PASS" in output
 
     def test_required_config_failure(self):
@@ -57,16 +56,3 @@ class ConfigTest(unittest.TestCase):
         confluent.controlcenter.internal.topics.replication=1
         """
         self.assertEquals(props.translate(None, string.whitespace), expected.translate(None, string.whitespace))
-
-    # def test_default_logging_config(self):
-    #     self.is_schema_registry_healthy_for_service("default-config")
-    #
-    #     log4j_props = self.cluster.run_command_on_service("default-config", "cat /etc/schema-registry/log4j.properties")
-    #     expected_log4j_props = """log4j.rootLogger=INFO, stdout
-    #
-    #         log4j.appender.stdout=org.apache.log4j.ConsoleAppender
-    #         log4j.appender.stdout.layout=org.apache.log4j.PatternLayout
-    #         log4j.appender.stdout.layout.ConversionPattern=[%d] %p %m (%c)%n
-    #
-    #         """
-    #     self.assertEquals(log4j_props.translate(None, string.whitespace), expected_log4j_props.translate(None, string.whitespace))
