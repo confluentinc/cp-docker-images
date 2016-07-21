8f8b080fdd3bdd052974b6db13897681d64cecab
Sumit Arrawatia <sumit.arrawatia@gmail.com>
2016-07-21 14:28:23 -0700

    Replace Oracle JDK with Zulu Openjdk 8 (#25)
    * Fix merge conflict in Makefile.
    
    * Replace Oracle JDK with Zulu OpenJDK and other base image fixes.
    
    - Replace Oracle JDK with Zulu Openjdk 8
    - Install python instead of depending on python-slim .This way we can
    pin down python/pip/debian version and not get surprises when
    python-slim changes.
    
    * Fix weird merge.

--- tests/test_build.py
+++ tests/test_build.py
@@ -21,13 +21,14 @@ class BaseImageTest(unittest.TestCase):
 
     def test_java_install(self):
         cmd = "java -version"
-        expected = 'java version "1.8.0_91"'
+        expected = 'OpenJDK Runtime Environment (Zulu 8.15.0.1-linux64) (build 1.8.0_92-b15)'
         output = utils.run_docker_command(image=self.image, command=cmd)
         self.assertTrue(expected in output)
 
     def test_dub_exists(self):
         self.assertTrue(utils.path_exists_in_image(self.image, "/usr/local/bin/dub"))
         self.assertTrue(utils.path_exists_in_image(self.image, "/usr/local/bin/cub"))
+
 
 class ZookeeperImageTest(unittest.TestCase):
 
@@ -94,24 +95,12 @@ class SchemaRegistryImageTest(unittest.TestCase):
         utils.build_image(self.image, get_dockerfile_path("debian/base"))
         utils.build_image(self.image, get_dockerfile_path("debian/schema-registry"))
 
-
-class ConnectImageTest(unittest.TestCase):
-
-    def setUp(self):
-        self.image = "confluentinc/kafka-connect"
-        utils.build_image(self.image, get_dockerfile_path("debian/base"))
-        utils.build_image(self.image, get_dockerfile_path("debian/kafka"))
-        utils.build_image(self.image, get_dockerfile_path("debian/kafka-connect"))
-
     def test_image_build(self):
         self.assertTrue(utils.image_exists(self.image))
 
-    def test_zk_install(self):
-        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/kafka"))
+    def test_schema_registry_install(self):
         self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent"))
         self.assertTrue(utils.path_exists_in_image(self.image, "/etc/schema-registry"))
-        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/kafka-connect"))
-
 
     def test_boot_scripts_present(self):
         self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/configure"))
@@ -127,3 +116,29 @@ class ConnectImageTest(unittest.TestCase):
         expected = "ERROR Properties file is required to start the schema registry REST instance (io.confluent.kafka.schemaregistry.rest.SchemaRegistryMain:38)"
         self.assertTrue(expected in utils.run_docker_command(image=self.image, command="schema-registry-start"))
 
+
+class ConnectImageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.image = "confluentinc/kafka-connect"
+        utils.build_image(self.image, get_dockerfile_path("debian/base"))
+        utils.build_image(self.image, get_dockerfile_path("debian/kafka"))
+        utils.build_image(self.image, get_dockerfile_path("debian/kafka-connect"))
+
+    def test_image_build(self):
+        self.assertTrue(utils.image_exists(self.image))
+
+    def test_connect_install(self):
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/kafka"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/kafka-connect"))
+
+    def test_boot_scripts_present(self):
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/configure"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/ensure"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/launch"))
+        self.assertTrue(utils.path_exists_in_image(self.image, "/etc/confluent/docker/run"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/configure"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/ensure"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/launch"))
+        self.assertTrue(utils.executable_exists_in_image(self.image, "/etc/confluent/docker/run"))
