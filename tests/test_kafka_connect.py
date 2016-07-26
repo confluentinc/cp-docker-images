2d6b749b6b7801a945d4c1911a3bda5aeccd05e8
arrawatia <sumit.arrawatia@gmail.com>
2016-07-25 18:40:48 -0700

    Basic test work. Pick it up back at home.

--- tests/test_kafka_connect.py
+++ tests/test_kafka_connect.py
@@ -11,6 +11,19 @@ KAFKA_READY = "bash -c 'cub kafka-ready $KAFKA_ZOOKEEPER_CONNECT {brokers} 20 20
 CONNECT_HEALTH_CHECK = "bash -c 'curl -X GET --fail --silent {host}:{port}/connectors && echo PASS || echo FAIL'"
 ZK_READY = "bash -c 'cub zk-ready {servers} 10 10 2 && echo PASS || echo FAIL'"
 SR_READY = "bash -c 'cub sr-ready {host} {port} 20 && echo PASS || echo FAIL'"
+FILE_SOURCE_CONNECTOR_CREATE = """
+    curl -X POST -H "Content-Type: application/json" \
+    --data '{"name": "%s", "config": {"connector.class":"org.apache.kafka.connect.file.FileStreamSourceConnector", "tasks.max":"1", "topic":"%s", "file": "%s"}}' \
+    http://%s:%s/connectors
+"""
+
+FILE_SINK_CONNECTOR_CREATE = """
+    curl -X POST -H "Content-Type: application/json" \
+    --data '{"name": "%s", "config": {"connector.class":"org.apache.kafka.connect.file.FileStreamSinkConnector", "tasks.max":"1", "topics":"%s", "file": "%s"}}' \
+    http://%s:%s/connectors
+"""
+
+CONNECTOR_STATUS = "curl -s -X GET http://{host}:{port}/connectors/{name}/status"
 
 
 class ConfigTest(unittest.TestCase):
@@ -20,16 +33,16 @@ class ConfigTest(unittest.TestCase):
         machine_name = os.environ["DOCKER_MACHINE_NAME"]
         cls.machine = utils.TestMachine(machine_name)
 
-        cls.cluster = utils.TestCluster("config-test", FIXTURES_DIR, "standalone-config.yml")
+        cls.cluster = utils.TestCluster("config-test", FIXTURES_DIR, "distributed-config.yml")
         cls.cluster.start()
 
         assert "PASS" in cls.cluster.run_command_on_service("zookeeper", ZK_READY.format(servers="localhost:2181"))
         assert "PASS" in cls.cluster.run_command_on_service("kafka", KAFKA_READY.format(brokers=1))
         assert "PASS" in cls.cluster.run_command_on_service("schema-registry", SR_READY.format(host="schema-registry", port="8081"))
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.cluster.shutdown()
+    @classmethod
+    def tearDownClass(cls):
+        cls.cluster.shutdown()
 
     @classmethod
     def is_connect_healthy_for_service(cls, service):
@@ -76,3 +89,83 @@ class ConfigTest(unittest.TestCase):
 
             """
         self.assertEquals(log4j_props.translate(None, string.whitespace), expected_log4j_props.translate(None, string.whitespace))
+
+
+class StandaloneNetworkingTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.cluster = utils.TestCluster("distributed-single-node", FIXTURES_DIR, "distributed-single-node.yml")
+        cls.cluster.start()
+        # assert "PASS" in cls.cluster.run_command_on_service("zookeeper-bridge", ZK_READY.format(servers="localhost:2181"))
+        assert "PASS" in cls.cluster.run_command_on_service("zookeeper-host", ZK_READY.format(servers="localhost:32181"))
+        # assert "PASS" in cls.cluster.run_command_on_service("kafka-bridge", KAFKA_READY.format(brokers=1))
+        assert "PASS" in cls.cluster.run_command_on_service("kafka-host", KAFKA_READY.format(brokers=1))
+        assert "PASS" in cls.cluster.run_command_on_service("schema-registry-host", SR_READY.format(host="localhost", port="8081"))
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #     cls.cluster.shutdown()
+
+    @classmethod
+    def is_connect_healthy_for_service(cls, service):
+        output = cls.cluster.run_command_on_service(service, CONNECT_HEALTH_CHECK.format(host="localhost", port=28082))
+        assert "PASS" in output
+
+    def test_host_network(self):
+        # Test from within the container
+        self.is_connect_healthy_for_service("connect-host")
+
+        # Create a file
+        record_count = 10000
+
+        # utils.run_docker_command(
+        #     image="confluentinc/cp-kafka-connect",
+        #     command="bash -c 'rm -rf /tmp/test/sink.test.txt && seq {count} > /tmp/test/source.test.txt'".format(count=record_count),
+        #     host_config={'NetworkMode': 'host', 'Binds': ['/tmp/connect-file-test/:/tmp/test']})
+        #
+        # utils.run_docker_command(
+        #     image="confluentinc/cp-kafka-connect",
+        #     command=FILE_SOURCE_CONNECTOR_CREATE % ("one-node-source-test", "one-node-test", "/tmp/test/source.test.txt", "localhost", "28082"),
+        #     host_config={'NetworkMode': 'host'})
+        #
+        # time.sleep(5)
+        #
+        # utils.run_docker_command(
+        #     image="confluentinc/cp-kafka-connect",
+        #     command=FILE_SINK_CONNECTOR_CREATE % ("one-node-sink-test", "one-node-test", "/tmp/test/sink.test.txt", "localhost", "28082"),
+        #     host_config={'NetworkMode': 'host'})
+        #
+        # time.sleep(5)
+        #
+        # source_logs = utils.run_docker_command(
+        #     image="confluentinc/cp-kafka-connect",
+        #     command=CONNECTOR_STATUS.format(host="localhost", port="28082", name="one-node-source-test"),
+        #     host_config={'NetworkMode': 'host'})
+        #
+        # source_connector = json.loads(source_logs)
+        #
+        # self.assertEquals(source_connector["connector"]["state"], "RUNNING")
+        #
+        # sink_logs = utils.run_docker_command(
+        #     image="confluentinc/cp-kafka-connect",
+        #     command=CONNECTOR_STATUS.format(host="localhost", port="28082", name="one-node-sink-test"),
+        #     host_config={'NetworkMode': 'host'})
+        #
+        # sink_connector = json.loads(sink_logs)
+        #
+        # self.assertEquals(sink_connector["connector"]["state"], "RUNNING")
+        #
+        # logs = utils.run_docker_command(
+        #     image="confluentinc/cp-kafka-connect",
+        #     command="bash -c '[ -e /tmp/test/sink.test.txt ] && echo PASS || echo FAIL' ",
+        #     host_config={'NetworkMode': 'host', 'Binds': ['/tmp/connect-file-test/:/tmp/test']})
+        #
+        # self.assertTrue("PASS" in logs)
+
+        sink_record_count = utils.run_docker_command(
+            image="confluentinc/cp-kafka-connect",
+            command="bash -c 'wc -l /tmp/test/sink.test.txt | cut -d\" \" -f1'",
+            host_config={'NetworkMode': 'host', 'Binds': ['/tmp/connect-file-test/:/tmp/test']})
+
+        self.assertEquals(sink_record_count, record_count)
