4581ef0e74634d25b675d9803f9f821c55e30c3d
Sumit Arrawatia <sumit.arrawatia@gmail.com>
2016-08-24 13:42:30 -0700

    Support SASL for Zookeeper. (#79)
    * Config tests pass.
    
    * Replace cub with Java version + SASL tests.
    
    * Kafka + ZK SASL tests pass.
    
    * Build docker-utils.
    
    * Add inline docs for cub and dub.
    
    Fixes #42.
    
    * Add java code from docker-utils.
    
    * Support zk for kafka-ready.
    
    * ZK, Kafka, SR tests run.
    
    * Rest Proxy and connect tests work.
    
    * Control center tests pass !
    
    * Cleanup and tests for cub.
    
    * Some more inline docs.
    
    * Ignore hacking code.
    
    * Changes for comments from @gwenshap and @theduderog
    
    Remove the Connectivity checking code as it was not doing anything. I
    think I did that because the python code had that. As @gwenshap
    mentioned the ZK Client code retires when the server is unavailable.
    The Kafka client fails but my code retries anyways, so that is also
    fine. The other condition that I though it guarded against was when a
    host was not resolvable but that  will never happen in properly
    configured docker/kubernetes environments (the DNS will always resolve
    even if the containers are not ready) and the check should fail if
    DNS/hostnames is not configured properly. And all the tests pass so I
    don’t think it is required.
    
    * Make changes as per @cotedm comments.
    
    Fix docs
    Fix logging
    Removed the properties as I have a command to generate it now :)
    Remove the zktimeout multiplier. I realized that I did that to debug a
    issue and forgot to remove it. If the ZK session times ou t during an
    operation, I think it is ok to fail as zkTimeout = timeout for the
    entire command set by the user.
    
    * Fixed typo.
    
    * Fixes for @cotedm comments.
    
    * Integrate tests into the makefile.
    
    * Refactor test-all make target.
    
    * Fix bug (KAFKA_OPTS will never be passed).
    
    * Fix grammatical errors in logging messages.
    
    * Fixed flaky tests and make ZK SASL tests work.
    
    - The SSL tests for Kafka are flaky because the consumer rebalance take
    longer than docker-compose timeout sometimes. Replaced console consumer
    with Kafkacat.
    
    - ZK SASL : The zkClient somehow gets all IPs associated with the
    hostnames for a given connect string .
    
    In this, case localhost was resolved to the VM hostname (as docker host
    networking just uses the host n/w stack). This causes problems with
    SASL as principal names are tied to hostnames. So, I replaced localhost
    with IP (127.0.0.1) and also changed localhost in the principal names
    to IP (127.0.0.1)
    
    Note that this lookup also causes issues with the bridged n/w and I
    could not find a workaround for it.
    
    * Fixes issue with SASL + ZKClient.
    
    The ZKClient needs a hostname that supports both DNS->IP and
    IP->Lookups. To make sure this condition is met,  I created a
    /etc/hosts that maps to eth0.

---
+++ java/src/test/bin/cli-test.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o nounset \
+    -o verbose \
+    -o xtrace
+
+ps ax | grep -i 'io.confluent.admin.utils.EmbeddedKafkaCluster' | grep -v grep | awk '{print $1}' | xargs kill -SIGTERM
+
+mvn clean compile package assembly:single -DskipTests=true
+
+export JAAS_CONF=/tmp/jaas.conf
+export MINI_KDC_DIR=/tmp/minikdc
+
+rm -rf "$MINI_KDC_DIR" && mkdir -p "$MINI_KDC_DIR"
+
+java -cp target/docker-utils-1.0.0-SNAPSHOT-tests.jar:$( mvn dependency:build-classpath | grep -v INFO) \
+    io.confluent.admin.utils.EmbeddedKafkaCluster \
+    3 \
+    3 \
+    true \
+    /tmp/client.properties \
+    "$JAAS_CONF" \
+    "$MINI_KDC_DIR" &>/tmp/test-kafka-cluster.log &
+
+KAFKA_PID=$!
+
+# Wait for Kafka to get ready. This is required because ZK Client fails (with AuthFailed event) if the MiniKDC is not up.
+sleep 10
+
+echo "Kafka : $KAFKA_PID"
+
+(java -jar target/docker-utils-1.0.0-SNAPSHOT-jar-with-dependencies.jar || exit 0)
+
+java -Djava.security.auth.login.config="$JAAS_CONF" \
+     -Djava.security.krb5.conf="$MINI_KDC_DIR/krb5.conf" \
+     -jar target/docker-utils-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+     zk-ready \
+     localhost:11117 \
+     30000 &> /tmp/test-zookeeper-ready.log
+
+ZOOKEEPER_TEST=$([ $? -eq 0 ] && echo "PASS" || echo "FAIL")
+
+java -Djava.security.auth.login.config="$JAAS_CONF" \
+     -Djava.security.krb5.conf="$MINI_KDC_DIR/krb5.conf" \
+     -jar target/docker-utils-1.0.0-SNAPSHOT-jar-with-dependencies.jar  \
+     kafka-ready \
+     3 \
+     30000 \
+     -b localhost:49092 \
+     --config /tmp/client.properties &> /tmp/test-kafka-ready.log
+
+KAFKA_BROKER_OPTION_TEST=$([ $? -eq 0 ] && echo "PASS" || echo "FAIL")
+
+java -Djava.security.auth.login.config="$JAAS_CONF" \
+     -Djava.security.krb5.conf="$MINI_KDC_DIR/krb5.conf" \
+     -jar target/docker-utils-1.0.0-SNAPSHOT-jar-with-dependencies.jar  \
+     kafka-ready \
+     3 \
+     30000 \
+     -z localhost:11117 \
+     -s SASL_SSL \
+     --config /tmp/client.properties &> /tmp/test-kafka-zk-ready.log
+
+KAFKA_ZK_OPTION_TEST=$([ $? -eq 0 ] && echo "PASS" || echo "FAIL")
+
+kill "$KAFKA_PID"
+
+# Wait for Kafka to die.
+sleep 10
+
+echo "TEST RESULTS:"
+echo "ZOOKEEPER_TEST=$ZOOKEEPER_TEST"
+echo "KAFKA_ZK_OPTION_TEST=$KAFKA_ZK_OPTION_TEST"
+echo "KAFKA_BROKER_OPTION_TEST=$KAFKA_BROKER_OPTION_TEST"
+
+[ ${ZOOKEEPER_TEST} == "PASS" ] \
+    && [ ${KAFKA_ZK_OPTION_TEST} == "PASS" ] \
+    && [ ${KAFKA_BROKER_OPTION_TEST} == "PASS" ] \
+    && exit 0 \
+    || exit 1
