f0591b61c3f102cde04882a0891529d7cfde90a8
Sumit Arrawatia <sumit.arrawatia@gmail.com>
2017-03-03 15:49:55 -0800

    Fix JMX version.

--- tests/test_zookeeper.py
+++ tests/test_zookeeper.py
@@ -202,7 +202,7 @@ class StandaloneNetworkingTest(unittest.TestCase):
             image="confluentinc/cp-jmxterm",
             command=JMX_CHECK.format(client_port=52181, jmx_hostname="localhost", jmx_port="39999"),
             host_config={'NetworkMode': 'host'})
-        self.assertTrue("Version = 3.4.8--1, built on 02/06/2016 03:18 GMT;" in logs)
+        self.assertTrue("Version = 3.4.9-1757313, built on 08/23/2016 06:50 GMT;" in logs)
 
     def test_jmx_bridged_network(self):
 
@@ -211,7 +211,7 @@ class StandaloneNetworkingTest(unittest.TestCase):
             image="confluentinc/cp-jmxterm",
             command=JMX_CHECK.format(client_port=2181, jmx_hostname="bridge-network-jmx", jmx_port="9999"),
             host_config={'NetworkMode': 'standalone-network-test_zk'})
-        self.assertTrue("Version = 3.4.8--1, built on 02/06/2016 03:18 GMT;" in logs)
+        self.assertTrue("Version = 3.4.9-1757313, built on 08/23/2016 06:50 GMT;" in logs)
 
 
 class ClusterBridgeNetworkTest(unittest.TestCase):
