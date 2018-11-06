1bf668f29269deebef46cc89cab27d0ed3cfcde3
Yeva Byzek <yeva@confluent.io>
2018-11-06 11:36:45 -0500

    Tweak start/stop script

--- examples/multi-datacenter/start.sh
+++ examples/multi-datacenter/start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker-compose up -d
 echo "sleeping 40s"
 sleep 40
@@ -7,16 +9,27 @@ sleep 40
 echo -e "\nsleeping 60s"
 sleep 60
 
-echo -e "\nDC1"
+echo -e "\n-----DC1-----"
+echo -e "\nlist topics:"
 docker-compose exec broker-dc1 kafka-topics --list --zookeeper zookeeper-dc1:2181
+echo -e "\ntopic1:"
 docker-compose exec schema-registry-dc1 kafka-avro-console-consumer --topic topic1 --bootstrap-server broker-dc1:9091 --property schema.registry.url=http://schema-registry-dc1:8081 --max-messages 10
-#docker-compose exec broker-dc1 kafka-run-class kafka.tools.DumpLogSegments --print-data-log --files /var/lib/kafka/data/topic1-0/00000000000000000000.log 
-#docker-compose exec replicator-dc1-to-dc2 bash -c "export CLASSPATH=/usr/share/java/kafka-connect-replicator/kafka-connect-replicator-5.1.0-SNAPSHOT.jar && kafka-console-consumer --topic topic1 --bootstrap-server broker-dc1:9091 --max-messages 5 --formatter=io.confluent.connect.replicator.tools.ProvenanceHeaderFormatter"
+echo -e "\n_schemas:"
 docker-compose exec broker-dc1 kafka-console-consumer --topic _schemas --bootstrap-server broker-dc1:9091 --from-beginning --timeout-ms 5000
+echo -e "\nprovenance info:"
+docker-compose exec replicator-dc1-to-dc2 bash -c "export CLASSPATH=/usr/share/java/kafka-connect-replicator/kafka-connect-replicator-5.1.0-SNAPSHOT.jar && kafka-console-consumer --topic topic1 --bootstrap-server broker-dc1:9091 --max-messages 10 --formatter=io.confluent.connect.replicator.tools.ProvenanceHeaderFormatter"
+echo -e "\ncluster id:"
+docker-compose exec zookeeper-dc1 zookeeper-shell localhost:2181 get /cluster/id | grep version | grep id | jq -r .id
 
-echo -e "\nDC2"
+echo -e "\n-----DC2-----"
+echo -e "\nlist topics:"
 docker-compose exec broker-dc2 kafka-topics --list --zookeeper zookeeper-dc2:2182
+echo -e "\ntopic1:"
 docker-compose exec schema-registry-dc1 kafka-avro-console-consumer --topic topic1 --bootstrap-server broker-dc2:9092 --property schema.registry.url=http://schema-registry-dc1:8081 --max-messages 10
-#docker-compose exec broker-dc2 kafka-run-class kafka.tools.DumpLogSegments --print-data-log --files /var/lib/kafka/data/topic1-0/00000000000000000000.log 
-#docker-compose exec replicator-dc1-to-dc2 bash -c "export CLASSPATH=/usr/share/java/kafka-connect-replicator/kafka-connect-replicator-5.1.0-SNAPSHOT.jar && kafka-console-consumer --topic topic1 --bootstrap-server broker-dc2:9092 --max-messages 5 --formatter=io.confluent.connect.replicator.tools.ProvenanceHeaderFormatter"
+echo -e "\n_schemas:"
 docker-compose exec broker-dc1 kafka-console-consumer --topic _schemas --bootstrap-server broker-dc2:9092 --from-beginning --timeout-ms 5000
+echo -e "\nprovenance info:"
+docker-compose exec replicator-dc1-to-dc2 bash -c "export CLASSPATH=/usr/share/java/kafka-connect-replicator/kafka-connect-replicator-5.1.0-SNAPSHOT.jar && kafka-console-consumer --topic topic1 --bootstrap-server broker-dc2:9092 --max-messages 10 --formatter=io.confluent.connect.replicator.tools.ProvenanceHeaderFormatter"
+echo -e "\ncluster id:"
+docker-compose exec zookeeper-dc2 zookeeper-shell localhost:2182 get /cluster/id | grep version | grep id | jq -r .id
+
--- examples/multi-datacenter/stop.sh
+++ examples/multi-datacenter/stop.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker-compose down
 #docker stop $(docker ps -a -q)
 #docker rm $(docker ps -a -q)
