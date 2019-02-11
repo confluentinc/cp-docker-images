28861f152f78a9135f3b3a110f97be0272fdfb03
ybyzek <ybyzek@users.noreply.github.com>
2019-02-11 15:58:00 -0500

    DEVX-662: Monitoring Replicator in Control Center (#676)

--- examples/multi-datacenter/submit_replicator_dc1_to_dc2_topic2.sh
+++ examples/multi-datacenter/submit_replicator_dc1_to_dc2_topic2.sh
@@ -3,21 +3,24 @@
 HEADER="Content-Type: application/json"
 DATA=$( cat << EOF
 {
-  "name": "replicator-dc1-to-dc2-topic1",
+  "name": "replicator-dc1-to-dc2-topic2",
   "config": {
     "connector.class": "io.confluent.connect.replicator.ReplicatorSourceConnector",
-    "topic.whitelist": "topic1,_schemas",
+    "topic.whitelist": "topic2",
+    "topic.rename.format": "\${topic}.replica",
     "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
     "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
     "src.kafka.bootstrap.servers": "broker-dc1:9091",
-    "src.consumer.group.id": "replicator-dc1-to-dc2-topic1",
+    "src.consumer.group.id": "replicator-dc1-to-dc2-topic2",
     "src.consumer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor",
     "src.consumer.confluent.monitoring.interceptor.bootstrap.servers": "broker-dc2:9092",
     "src.kafka.timestamps.producer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor",
     "src.kafka.timestamps.producer.confluent.monitoring.interceptor.bootstrap.servers": "broker-dc2:9092",
     "dest.kafka.bootstrap.servers": "broker-dc2:9092",
     "dest.kafka.replication.factor": 1,
-    "provenance.header.enable": "true",
+    "provenance.header.enable": "false",
+    "offset.timestamps.commit": "true",
+    "offset.topic.commit": "true",
     "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
     "tasks.max": "1"
   }
