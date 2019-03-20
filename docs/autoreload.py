479aee42255b2e324f0c014ac18aedc764b37fbc
Joel Hamill <11722533+joel-hamill@users.noreply.github.com>
2019-03-20 15:52:07 -0700

    DOCS-1290 - Move /docs to confluentinc/docs repository (#707)

--- docs/autoreload.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from livereload import Server, shell
-
-server = Server()
-server.watch('*.rst', shell('make html'))
-server.watch('*/*.rst', shell('make html'))
-server.serve()
