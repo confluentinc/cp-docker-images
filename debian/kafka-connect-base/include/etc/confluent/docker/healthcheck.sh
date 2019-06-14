147a4c852006d443ea8b069d6b9db02695da4e45
Robin Moffatt <robin@confluent.io>
2019-06-14 11:57:08 +0100

    Add healthcheck for Kafka Connect

---
+++ debian/kafka-connect-base/include/etc/confluent/docker/healthcheck.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ $(curl -s -o /dev/null -w %{http_code} http://localhost:$CONNECT_REST_PORT/) = 200 ]]; then
+  echo "Woohoo! Kafka Connect is up!"
+  exit 0
+else 
+  echo -e $(date) "\tKafka Connect HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://localhost:$CONNECT_REST_PORT/info) " (waiting for 200)"
+  exit 1
+fi
