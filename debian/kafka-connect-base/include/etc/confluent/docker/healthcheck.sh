72646347fd636b18e687f700cdb53c3a3cb2be80
Robin Moffatt <robin@confluent.io>
2019-06-14 12:00:20 +0100

    use CONNECT_REST_ADVERTISED_HOST_NAME instead of localhost

--- debian/kafka-connect-base/include/etc/confluent/docker/healthcheck.sh
+++ debian/kafka-connect-base/include/etc/confluent/docker/healthcheck.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ $(curl -s -o /dev/null -w %{http_code} http://localhost:$CONNECT_REST_PORT/) = 200 ]]; then
+if [[ $(curl -s -o /dev/null -w %{http_code} http://$CONNECT_REST_ADVERTISED_HOST_NAME:$CONNECT_REST_PORT/) = 200 ]]; then
   echo "Woohoo! Kafka Connect is up!"
   exit 0
 else 
-  echo -e $(date) "\tKafka Connect HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://localhost:$CONNECT_REST_PORT/info) " (waiting for 200)"
+  echo -e $(date) "\tKafka Connect HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://$CONNECT_REST_ADVERTISED_HOST_NAME:$CONNECT_REST_PORT/info) " (waiting for 200)"
   exit 1
 fi
